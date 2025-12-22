82c1ab4126871894006e05f0e3f13a63d9ac642c
Dawn <hyshen_olvae@zohomail.com>
2025-12-22 10:56:21 +0800

    feat: 加入一个低代码平台框架

---
+++ Peidi-LowCode-Grapes/plugins/grapesjs-loading.js
@@ -0,0 +1,51 @@
+function applyStyle(el, style) {
+    Object.keys(style).forEach(key => {
+        el.style[key] = style[key];
+    });
+}
+export default (editor, options = {}) => {
+    const defaultOpts = {
+        appendTo: document.body,
+        start: true,
+        style: {
+            padding: '4px',
+            backgroundColor: 'white',
+            opacity: .6,
+            position: 'fixed',
+            zIndex: 10,
+            transition: 'width 0.2s ease-out',
+            pointerEvents: 'none',
+        },
+        visibleStyle: {
+            visibility: 'visible',
+            width: '100%',
+        },
+        hiddenStyle: {
+            visibility: 'hidden',
+            width: '0%',
+        }
+    };
+    const opts = Object.assign(defaultOpts, options);
+    // Create element
+    const loadingEl = document.createElement('div');
+    editor.on('load', () => {
+        const container = typeof opts.appendTo === 'string' ? document.querySelector(opts.appendTo) : opts.appendTo;
+        if (container) {
+            if (opts.start)
+                container.insertBefore(loadingEl, container.firstChild);
+            else
+                container.appendChild(loadingEl);
+        }
+    });
+    // Style
+    applyStyle(loadingEl, opts.style);
+    // Loading visibility
+    loadingEl.style.visibility = 'hidden';
+    editor.on('storage:start', () => {
+        applyStyle(loadingEl, opts.visibleStyle);
+    });
+    editor.on('storage:end', () => {
+        applyStyle(loadingEl, opts.hiddenStyle);
+    });
+};
+//# sourceMappingURL=index.js.map
